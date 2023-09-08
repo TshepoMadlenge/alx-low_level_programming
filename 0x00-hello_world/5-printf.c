a92b8b880805adf24ceec5e22a5f3b02a58a94f5
Tshepo <tshepomadlenge@gmail.com>
2023-09-08 08:27:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a pgram that prints a line using printf function
+ * return : 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+
